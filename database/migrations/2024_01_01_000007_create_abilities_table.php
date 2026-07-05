5634d0483122c3e9c4de54c4175f6617e4840577
Lantum-Brendan <lantumbrendan170@gmail.com>
2026-07-05 12:25:45 +0100

    fix: Restore migration, composer, and config changes

--- database/migrations/2024_01_01_000007_create_abilities_table.php
+++ database/migrations/2024_01_01_000007_create_abilities_table.php
@@ -11,9 +11,15 @@ return new class extends Migration
         Schema::create('abilities', function (Blueprint $table) {
             $table->id();
             $table->string('action');
-            $table->nullableMorphs('subject');
-            $table->morphs('assignable');
-            $table->nullableMorphs('context');
+            if (config('roles.use_uuids')) {
+                $table->nullableUuidMorphs('subject');
+                $table->uuidMorphs('assignable');
+                $table->nullableUuidMorphs('context');
+            } else {
+                $table->nullableMorphs('subject');
+                $table->morphs('assignable');
+                $table->nullableMorphs('context');
+            }
             $table->boolean('allowed')->default(true);
             $table->json('conditions')->nullable();
             $table->timestamps();
